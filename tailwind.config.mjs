c13fd2ace576d8ce43cb91a519e6700315bf7097
greysmpich <greys.mpich@gmail.com>
2024-02-27 20:58:33 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		colors:{
+			'green-greys': '#23EDB1',
+			'pink-greys': '#FF0D75',
+			'lightPink-greys': '#FACED9',
+			'white': '#f0fdf4',
+			"black": "#27272a"
+		},
+		extend: {},
+	},
+	plugins: [],
+}
